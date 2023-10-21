f9fde9c31271cfe4a5b99a613c826a2839e9869b
Nicole308 <91211275+Nicole308@users.noreply.github.com>
2023-10-21 11:40:47 -0700

    added request headers

--- backend/index.js
+++ backend/index.js
@@ -34,9 +34,9 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Origin', ['*']);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 // Add the client URL to the CORS policy
