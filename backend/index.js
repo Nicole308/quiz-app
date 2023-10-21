39db7d65ce49576602931138d3b105b5461fdd63
Nicole308 <91211275+Nicole308@users.noreply.github.com>
2023-10-20 23:31:49 -0700

    Edited the cors policy

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,12 @@ mongoose.connect(MONGO_URL, {dbName:'quizUsers', useNewUrlParser: true}).then(()
 
 const app = express()
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // app.use(express.json())
 app.use(bodyParser.json())
 app.use(cookieParser(process.env.COOKIE_SECRET))
